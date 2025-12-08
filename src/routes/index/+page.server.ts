4cbfbe216e4182518d30ef1ecf97e56040c58c87
Ryo-Soikutsu <ryo-soikutsu@northland.dev>
2025-12-08 14:55:12 +0800

    Updated index page to remove references to old blog page

--- src/routes/index/+page.server.ts
+++ src/routes/index/+page.server.ts
@@ -1,4 +1,3 @@
-import { load as loadPosts } from '$src/routes/blog/+page.server';
 import { load as loadProjects } from '$src/routes/projects/+page.server';
 import { load as loadSocials } from '$src/routes/contact/+page.server';
 import config from '$src/helpers/config';
@@ -9,7 +8,6 @@ import type { PageServerLoad } from './$types';
 /** @type {import('./$types').PageLoad} */
 export const load = async ({ fetch }: PageServerLoad) => {
   return {
-    posts: (await loadPosts({ fetch }, config.initialFeeds)).posts,
     repos: (await loadProjects({ fetch })).repos,
     socials: (await loadSocials({ fetch })).props,
   };
