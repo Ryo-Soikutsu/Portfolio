f4ee5951a23753c26f675d4738a0b440da40d6d0
Ryo Soikutsu <64062496+Ryo-Soikutsu@users.noreply.github.com>
2025-12-11 18:43:34 +0800

    Test preview deployment

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,17 @@
-// import { redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
-// import config from '$src/helpers/config';
-// import * as publicEnvs from '$env/static/public';
+import config from '$src/helpers/config';
+import * as publicEnvs from '$env/static/public';
 
-// // If the app is configured to use the /index route instead of home, then redirect
-// export function load() {
-//   const shouldLoadIndex = publicEnvs?.PUBLIC_LOAD_INDEX || config?.loadIndex;
-//   if (shouldLoadIndex) {
-//     throw redirect(302, '/index');
-//   }
-// };
+// If the app is configured to use the /index route instead of home, then redirect
+export function load() {
+  const shouldLoadIndex = publicEnvs?.PUBLIC_LOAD_INDEX || config?.loadIndex;
+  if (shouldLoadIndex) {
+    throw redirect(302, '/index');
+  }
+};
 
+/*
 import { load as loadProjects } from '$src/routes/projects/+page.server';
 import { load as loadSocials } from '$src/routes/contact/+page.server';
 import config from '$src/helpers/config';
@@ -19,9 +20,11 @@ import type { PageServerLoad } from './$types';
 export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
+/*
 export const load = async ({ fetch }: PageServerLoad) => {
   return {
     repos: (await loadProjects({ fetch })).repos,
     socials: (await loadSocials({ fetch })).props,
   };
 };
+*/
